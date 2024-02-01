6428a6ede87be44751d3a5a61c479a970b44ced6
vineethsankre <113631587+vineethsankre@users.noreply.github.com>
2024-02-02 00:19:35 +0530

    Time: 2 ms (93.56%), Space: 44.8 MB (37.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -15,13 +15,5 @@ class Solution {
         return result;
         
     }
-    public static void main(String[] args){
-        int[] nums = {2, 7, 11, 15 };
-        int target = 9;
-        Solution solution = new Solution();
-        int[] result = solution.twoSum(nums, target);
-        System.out.println(Arrays.toString(result));
-        
-        
-    }
+
 }
