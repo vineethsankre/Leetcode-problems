12d0f0c1a81d8a905a509a82af17732c05657834
vineethsankre <113631587+vineethsankre@users.noreply.github.com>
2024-02-02 13:56:11 +0530

    Time: 1 ms (99.83%), Space: 56.9 MB (80.80%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,12 +3,12 @@ class Solution {
         int N = nums.length;
         int maxSoFar = nums[0];
         int curMax = nums[0];
-
+        
         for (int i = 1; i<N; i++){
             curMax = Math.max(nums[i], nums[i] + curMax);
             maxSoFar = Math.max(curMax, maxSoFar);
         }
-
         return maxSoFar;
+
     }
 }
